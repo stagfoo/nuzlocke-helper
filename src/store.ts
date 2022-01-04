48d8ae54fa9a6f9abbf889b9611e5c8bf77f2202
stagfoo <alexanderddking@gmail.com>
2022-01-05 07:32:40 +0900

    🪴　feature ░ immutable store logic

--- src/store.ts
+++ src/store.ts
@@ -1,31 +1,52 @@
 import { reducer } from 'obake.js';
 
+export type Pokemon = {
+  img: string,
+  name: string,
+  lvl: number,
+  id: string,
+  inBox: boolean;
+}
+
+export type State = {
+  currentPage: { name: string },
+  listedPokemon: Array<Pokemon>,
+  searchResults: Array<string>,
+  searchBox: string,
+  addNewPokemon: Pokemon
+}
+
 export const defaultState = {
-  bucket: '',
-  currentPage: { name: 'HOME', activePage: "/" },
-  notification: {
-    text: "",
-    show: false
+  currentPage: { name: 'HOME' },
+  listedPokemon: [],
+  searchResults: [],
+  searchBox: "",
+  addNewPokemon: {
+    id: "",
+    name: "",
+    lvl: 0,
+    img: "",
+    inBox: false
   }
- }
+}
 
- export const routes = {
-    'Home': '/',
-    'Example Fetch': '/example-fetch',
- }
-
- export const activePage = {
-   "HOME": "/",
-   "EXAMPLE_FETCH": "/example-fetch"
- }
 export const reducers = {
   updateCurrentPage: reducer((state, value: string) => {
-    state.currentPage = { name: value, activePage:activePage[value]  };
+    state.currentPage = { name: value };
   }),
-  updateBucket: reducer((state, value: string) => {
-    state.bucket = value;
+  updateNewPokemon: reducer((state, value: Pokemon) => {
+    state.addNewPokemon = value
   }),
-  updateNotification: reducer((state, value:{text: string, show: boolean}) => {
-    state.notification = value;
+  updateSearch: reducer((state, value: string) => {
+    state.searchBox = value
+  }),
+  updateSearchResults: reducer((state, value: Array<Pokemon>) => {
+    state.searchResults = value
+  }),
+  addToCurrentTeam: reducer((state, value: Pokemon) => {
+    state.listedPokemon = [value, ...state.listedPokemon]
+  }),
+  updateListedPokemon: reducer((state, value: Array<Pokemon>) => {
+    state.listedPokemon = value
   }),
 }

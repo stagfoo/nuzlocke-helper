c203bb6290da49ee9cd58398430c1ef9ce965bc8
stagfoo <alexanderddking@gmail.com>
2022-01-05 07:33:20 +0900

    🪴　feature ░  Local storage save/load

--- src/index.ts
+++ src/index.ts
@@ -1,19 +1,22 @@
 import { reducers, defaultState } from './store';
+import { saveState, loadState } from './actions';
 import { startRouters } from './url';
 import { createStore } from 'obake.js';
 import { AppRoot } from './ui';
-import { BaseStyles  } from './styles';
+import { BaseStyles } from './styles';
 import morph from 'nanomorph';
 
 //Default render
 const ROOT_NODE = document.body.querySelector('#app');
+export const storageKey = "pokemon_state";
 
 //Create Store
+const savedState = loadState(storageKey, defaultState)
 export const state = createStore(
-    defaultState,
-    { renderer },
-    reducers
-  );
+  savedState,
+  { renderer, saveState },
+  reducers
+);
 
 //Render Loop function
 // spec - https://dom.spec.whatwg.org/#concept-node-equals
@@ -22,19 +25,22 @@ function renderer(newState) {
     onBeforeElUpdated: (f, i) => !f.isEqualNode(i)
   })
 }
+
 //Start Router listener
 startRouters();
 BaseStyles();
 
+
+//Disable if you are doing changes locally
 if ('serviceWorker' in navigator) {
   window.addEventListener('load', () => {
-      navigator.serviceWorker.register('./service-worker.js').then(
-          registration => {
-              console.log(`ServiceWorker registration successful with scope: ${registration.scope}`);
-          },
-          error => {
-              console.log(`ServiceWorker registration failed: ${error}`);
-          }
-      );
+    navigator.serviceWorker.register('./service-worker.js').then(
+      registration => {
+        console.log(`ServiceWorker registration successful with scope: ${registration.scope}`);
+      },
+      error => {
+        console.log(`ServiceWorker registration failed: ${error}`);
+      }
+    );
   });
 }
